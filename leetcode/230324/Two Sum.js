4c1ed42ae0ee6ad4d8d27c073c957dc98cd0c627
슈피치 <shoutrawberry@gmail.com>
2023-03-24 02:05:11 +0900

    🍑 comment: Two Sum

---
+++ leetcode/230324/Two Sum.js
@@ -0,0 +1,23 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order. */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const arr = [];
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                arr[0] = i;
+                arr[1] = j;
+            }
+        }
+    }
+    return arr;
+};
+
+// Solution을 보니 Map을 이용한 코드가 많다 Map 공부 및 포스팅하기
