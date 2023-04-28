26eaba63546b9566381bb2cfdef0713dbcb9925c
슈피치 <shoutrawberry@gmail.com>
2023-04-28 23:28:38 +0900

    🍑 comment: 완주하지 못한 선수

--- programmers/level 1/230428/완주하지 못한 선수.js
+++ programmers/level 1/230428/완주하지 못한 선수.js
@@ -6,6 +6,12 @@
  * Algorithm: 해시
  */
 
+/* pseudocode
+   1. 참가자와 완주자에 해당하는 객체를 각각 만든다.
+   2. 각 객체에 이미 있는 이름이라면 1명을 더 더해준다. (동명이인 처리)
+   3. 참가자와 완주자 객체의 값이 동일하지 않은 경우 해당 키 값을 반환한다.
+*/
+
 function solution(participant, completion) {
   const part = {};
   const comp = {};
