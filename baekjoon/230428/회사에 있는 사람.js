3096ecbe701aec4d4f89aa0ff45a584c7c3e266f
슈피치 <shoutrawberry@gmail.com>
2023-04-28 13:43:05 +0900

    🍑 comment: 회사에 있는 사람
    memory: 45688KB
    time: 456ms

---
+++ baekjoon/230428/회사에 있는 사람.js
@@ -0,0 +1,45 @@
+/**
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net/problem/7785
+ * Problem Number: 7785
+ * Level: Silver V
+ * Algorithm: 자료 구조 / 해시를 사용한 집합과 맵
+ */
+
+const [N, ...input] = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+/* Use Object */
+const worker = {};
+
+for (const info of input) {
+  const [name, status] = info.split(' ');
+  worker[name] = status;
+}
+
+const name = Object.keys(worker);
+const entered = name.filter((key) => worker[key] === 'enter');
+
+entered.sort().reverse();
+
+for (const name of entered) {
+  console.log(name);
+}
+// 시간 초과로 정답 수정
+
+/* Use Set */
+const worker = new Set();
+
+for (const info of input) {
+  const [name, status] = info.split(' ');
+  if (status === 'enter') {
+    worker.add(name);
+  } else worker.delete(name);
+}
+
+const entered = [...worker].sort().reverse();
+
+console.log(entered.join('\n'));
