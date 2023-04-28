489164cafc3eae2ffee879fddf15ca46336c2cff
슈피치 <shoutrawberry@gmail.com>
2023-04-29 00:48:14 +0900

    🍑 comment: Hello World
    memory: 9264KB
    time: 108ms

---
+++ baekjoon/bronze/230429/Hello World.js
@@ -0,0 +1,13 @@
+/**
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net/problem/2557
+ * Problem Number: 2557
+ * Level: Bronze V
+ * Algorithm: 구현
+ */
+
+/* pseudocode
+   1. Hello World!를 출력한다.
+*/
+
+console.log('Hello World!');
