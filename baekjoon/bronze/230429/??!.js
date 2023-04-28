fd773739918f3e9ff42e3c73efb4b1663ee9a4c0
슈피치 <shoutrawberry@gmail.com>
2023-04-29 01:07:24 +0900

    🍑 comment: ??!
    memory: 9336KB
    time: 132ms

---
+++ baekjoon/bronze/230429/??!.js
@@ -0,0 +1,15 @@
+/**
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net/problem/10926
+ * Problem Number: 10926
+ * Level: Bronze V
+ * Algorithm: 구현
+ */
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+/* pseudocode
+   1. 입력값에 ??!를 붙여 출력한다.
+*/
+
+console.log(`${input}??!`);
