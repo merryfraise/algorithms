2e1115f8d9ae8961dd74b229146aba6b0b0f4bd6
슈피치 <shoutrawberry@gmail.com>
2023-04-29 00:45:11 +0900

    🍑 comment: A-B

---
+++ baekjoon/bronze/230429/A-B.js
@@ -0,0 +1,15 @@
+/**
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net/problem/1001
+ * Problem Number: 1001
+ * Level: Bronze V
+ * Algorithm: 수학 / 구현 / 사칙연산
+ */
+
+const [A, B] = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ");
+
+/* pseudocode
+   1. 두 값을 Number로 형변환하여 뺀다.
+*/
+
+console.log(Number(A) + Number(B));
