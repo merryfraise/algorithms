965ae07589875e4cf35119fd8102cec2a389d69c
슈피치 <shoutrawberry@gmail.com>
2023-05-01 22:23:54 +0900

    🍑 comment: 고양이
    memory: 9268KB
    time: 148ms

---
+++ baekjoon/bronze/230501/고양이.js
@@ -0,0 +1,16 @@
+/**
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net/problem/10171
+ * Problem Number: 10171
+ * Level: Bronze V
+ * Algorithm: 구현
+ */
+
+/* pseudocode
+   1. 백슬래시(\)를 문자열로 출력하기 위해서는 백슬래시를 2번 써주어야 한다.
+*/
+
+console.log(`\\    /\\`);
+console.log(` )  ( ')`);
+console.log(`(  /  )`);
+console.log(` \\(__)|`);
