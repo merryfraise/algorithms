8cbad6dcecd4e00e0073871968562b743527ba9e
슈피치 <shoutrawberry@gmail.com>
2023-05-01 22:27:30 +0900

    🍑 comment: 개
    memory: 9280KB
    time: 120ms

---
+++ baekjoon/bronze/230501/개.js
@@ -0,0 +1,18 @@
+/**
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net/problem/10172
+ * Problem Number: 10172
+ * Level: Bronze V
+ * Algorithm: 구현
+ */
+
+/* pseudocode
+   1. 백슬래시(\)를 문자열로 출력하기 위해서는 백슬래시를 2번 써주어야 한다.
+   2. 템플릿 리터럴을 사용했기 때문에 백틱(`)을 출력해야 하는 부분은 ${}로 감싸 문자열 처리를 한다.
+*/
+
+console.log(`|\\_/|`);
+console.log(`|q p|   /}`);
+console.log(`( 0 )"""\\`);
+console.log(`|"^"${'`'}    |`);
+console.log(`||_/=\\\\__|`);
