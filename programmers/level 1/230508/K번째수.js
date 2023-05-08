72cc35a1d76c830985ade420a78205c3b0d5bdf3
슈피치 <shoutrawberry@gmail.com>
2023-05-08 14:09:33 +0900

    🍑 comment: K번째수

--- programmers/level 1/230508/K번째수.js
+++ programmers/level 1/230508/K번째수.js
@@ -6,6 +6,13 @@
  * Algorithm: 정렬
  */
 
+/* pseudocode
+   1. commands를 순회하면서
+   2. array를 commands[0] - 1부터 commands[1]까지 slice를 한 뒤
+   3. slice한 배열을 오름차순으로 정렬하고
+   4. 정렬한 배열의 commands[2]번째 숫자를 sliced 배열에 push한 뒤 sliced를 리턴한다.
+*/
+
 function solution(array, commands) {
   const sliced = [];
 
