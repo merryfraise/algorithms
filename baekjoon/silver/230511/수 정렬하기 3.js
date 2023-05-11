d6f8805dde3ac2e83dad8d47cd175baca932e19f
슈피치 <shoutrawberry@gmail.com>
2023-05-11 14:13:13 +0900

    🍑 comment: 수 정렬하기 3
    mle

--- baekjoon/silver/230511/수 정렬하기 3.js
+++ baekjoon/silver/230511/수 정렬하기 3.js
@@ -14,12 +14,12 @@ const [N, ...input] = require('fs')
   .map(Number);
 
 /* pseudocode
-   1. 문제에 작성된 의사코드대로 quick sort를 정의
-   2. 요구하는 것은 K번째에 교환된 숫자를 작은 수부터 출력하는 것
-   3. 교환 횟수 카운팅을 위한 변수 생성 (count)
-   4. 교환된 숫자를 담기 위한 배열 생성 (result)
-   5. count가 K와 동일하다면 배열에 작은 수부터 push (Math.min, Math.max 이용)
-   6. 총 교환 횟수가 K보다 작다면 -1 출력 아니라면 result 출력
+   1. counting sort 이용
+   2. 배열에 요소가 몇개씩 있는지 세기 위한 count 배열 생성
+   3. 해당 요소의 인덱스 번호에 개수를 추가
+   4. 결과 배열에 count 배열을 재 배열 (빈 인덱스를 없애기 위해)
+   5. result를 한 줄씩 결과로 출력
+   6. 메모리 부족
 */
 
 const count = [];
